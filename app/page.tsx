9a0611760300cdb0b18e21e24bcafeeed05365e0
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-30 19:05:56 +0530

    apply smooth scroll for all pages

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   // const profile="https://sdk.bitmoji.com/me/sticker/jNLqpQKAbxdAnocFxJCTWyATyVeig0bGqzyNqTVZDdek92ETEx1toQ/10212044.png?p=dD1wO3Y9cG9wdWxhcjtsPWVu.v1&size=thumbnail"
   return (
-    <main className="px-8 py-8">
+    <main className="px-8 py-8 scroll-smooth">
 
       <HomePage/>
 
