56215a00e863d3abf271b3a9c2a673609d720b72
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-30 19:05:10 +0530

    make it responisive for sm and md devices

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -5,10 +5,10 @@ function Home() {
   return (
     <main>
       <NavBar />
-      <div className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-teal-500 via-cyan-600 to-blue-700 rounded-3xl w-full h-dvh">
+      <div className="bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] from-teal-500 via-cyan-600 to-blue-700 rounded-3xl w-auto h-auto">
         <div className="flex flex-col gap-4 font-manrope justify-start  px-10 py-40 lg:">
-          <p>Freelancer Desingner Based Sri Lanka</p>
-          <h1 className="  lg:text-8xl  max-md:text-6xl max-sm:4xl">
+          <p className="text-sm md:text-xl lg:text-2xl">Freelancer Desingner Based Sri Lanka</p>
+          <h1 className=" text-4xl lg:text-8xl  md:text-6xl">
             Building digital <br /> products, brands, & <br />
             experiences.
           </h1>
