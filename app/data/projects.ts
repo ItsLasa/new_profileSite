307b9970e0dfb9d50bcf1c85acb41ac4fbf77605
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-30 10:42:22 +0530

    add projects data details

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -2,18 +2,18 @@
 export interface Project {
     id: number;
     title: string;
-    
+    image: string;
   }
   
   const projects: Project[] = [
-    { id: 1, title: "Project One" },
-    { id: 2, title: "Project Two"},
-    { id: 3, title: "Project Three" },
-    { id: 4, title: "Project Four"},
-    { id: 5, title: "Project Five" },
-    { id: 6, title: "Project Six"},
-    { id: 7, title: "Project Seven" },
-    { id: 8, title: "Project Eight" },
+    { id: 1, title: "Project One", image: "/todo1.jpg" },
+    { id: 2, title: "Project Two", image: "/todo2.png"  },
+    { id: 3, title: "Project Three", image: "/public/todo1.jpg"  },
+    { id: 4, title: "Project Four", image: "/public/todo1.jpg"  },
+    { id: 5, title: "Project Five", image: "/public/todo1.jpg"  },
+    { id: 6, title: "Project Six", image: "/public/todo1.jpg" },
+    { id: 7, title: "Project Seven", image:"/public/todo1.jpg"  },
+    { id: 8, title: "Project Eight", image: "/public/todo1.jpg"  },
   ];
   
   export default projects;
