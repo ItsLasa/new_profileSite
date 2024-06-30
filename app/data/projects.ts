763ac553224cf03fef26081b0f31a8e4b0dd5584
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-30 10:26:30 +0530

    add project data for projects page

---
+++ app/data/projects.ts
@@ -0,0 +1,20 @@
+// data/projects.ts
+export interface Project {
+    id: number;
+    title: string;
+    
+  }
+  
+  const projects: Project[] = [
+    { id: 1, title: "Project One" },
+    { id: 2, title: "Project Two"},
+    { id: 3, title: "Project Three" },
+    { id: 4, title: "Project Four"},
+    { id: 5, title: "Project Five" },
+    { id: 6, title: "Project Six"},
+    { id: 7, title: "Project Seven" },
+    { id: 8, title: "Project Eight" },
+  ];
+  
+  export default projects;
+  
