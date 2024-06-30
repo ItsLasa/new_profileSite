02372eb2a9b59006a62ec01a6dda5df05712011b
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-06-30 19:04:19 +0530

    change style and make it modern view and animate hover

--- app/components/ProjectsCards.tsx
+++ app/components/ProjectsCards.tsx
@@ -1,6 +1,8 @@
 // components/ProjectCard.tsx
 import Image from "next/image";
 import { Project } from "../data/projects";
+import { FaLink } from "react-icons/fa";
+import Link from "next/link";
 
 interface ProjectCardProps {
   project: Project;
@@ -8,9 +10,24 @@ interface ProjectCardProps {
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
   return (
-    <div className="border rounded-lg shadow-lg p-4">
-      <Image src={project.image} alt={project.title} width={300} height={200} className="rounded-t-lg" />
-      <h3 className="text-xl font-semibold mt-4">{project.title}</h3>
+    <div className="border rounded-lg shadow-lg p-8">
+      <Image
+        src={project.image}
+        alt={project.title}
+        width={400}
+        height={200}
+        className="rounded-t-lg transition-all duration-300 hover:scale-105"
+      />
+
+      <div className=" border-gray-300 pt-4 border-b-[1px] "></div>
+
+      <div className="flex justify-between items-center px-2">
+        <h3 className=" text-lg mt-4 mb-4">{project.title}</h3>
+        <a href={project.link}>
+          <FaLink className="hover:scale-110  transition-all duration-300" />
+        </a>
+      </div>
+      <div className="flex text-sm text-zinc-500 font-manrope justify-start items-center px-2">{project.details}</div>
     </div>
   );
 };
