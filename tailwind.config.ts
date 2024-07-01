3d5e52d824c1f0d44dc94d6d1c369c5174e0121a
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-01 09:31:38 +0530

    add google font Fraunces  for tail fontFamily

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,21 @@
 import type { Config } from "tailwindcss";
 
+
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  
   theme: {
     extend: {
       fontFamily:{
 
        'manrope':[ "Manrope"," sans-serif"],
        'cormorant': ["Cormorant", "serif"],
+       'franunce': ["Fraunces", "serif"],
 
       },
       backgroundImage: {
