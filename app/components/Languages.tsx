a9e01629913ed807dbcfdda1b1ca7f2a67eb8444
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-01 09:33:21 +0530

    make language magicui components

---
+++ app/components/Languages.tsx
@@ -0,0 +1,42 @@
+import IconCloud from "./magicui/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "dart",
+  "java",
+  "react",
+  "flutter",
+  "android",
+  "html5",
+  "css3",
+  "nodedotjs",
+  "express",
+  "nextdotjs",
+  "prisma",
+  "amazonaws",
+  "postgresql",
+  "firebase",
+  "nginx",
+  "vercel",
+  "testinglibrary",
+  "jest",
+  "cypress",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "sonarqube",
+  "figma",
+];
+
+export default function IconCloudDemo() {
+  return (
+    <div className="relative mt-4 flex h-full w-full max-w-[28rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-16 pb-20 pt-8 ">
+      <IconCloud iconSlugs={slugs} />
+    </div>
+  );
+}
