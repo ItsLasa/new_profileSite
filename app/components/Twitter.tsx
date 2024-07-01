ad633a761568ca6b54d788af60827204d82cea47
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-01 09:33:44 +0530

    build twitt card

--- app/components/Twitter.tsx
+++ app/components/Twitter.tsx
@@ -1,15 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
+import { MdArrowOutward } from 'react-icons/md'
 import { SlSocialTwitter } from 'react-icons/sl'
 
 function Twitter() {
   return (
-    <div className=' shadow-lg rounded-lg p-8'>
-        <div className='flex justify-between items-center px-2'>
-           <Image src={'https://pbs.twimg.com/profile_images/1361782884558729216/Sm-FPj4w_400x400.jpg'} width={100} height={100} alt={''}/>
-           <div>
-           <SlSocialTwitter />
+    <div className='shadow-lg rounded-lg w-[350px] px-4 py-4 h-auto bg-[#F5F5F5] border   mt-4'>
+        <div className='flex justify-between items-center px-2 py-4'>
+           <Image src={'https://pbs.twimg.com/profile_images/1361782884558729216/Sm-FPj4w_400x400.jpg'} width={50} height={50} alt={''} className=' rounded-full'/>
+           <div className='px-4 py-4 rotate-3 bg-blue-400 rounded-lg '>
+           <SlSocialTwitter className='text-xl font-medium animate-pulse' />
            </div>
+        </div>
+        <div className='px-2  font-manrope text-2xl'>
+        i get computers ‘puting ✸ ai design <span className='text-orange-700'>@figma</span> ✸ prev @diagram
+        </div>
+        <div className='group mt-4 px-2 py-2 gap-2 flex justify-center items-center last: shadow-lg border-[1px] border-black rounded-xl transition-all duration-700 cursor-pointer'>
+            <button>Read my tweets </button>
+            <MdArrowOutward className=' group-hover:rotate-45 group-hover:scale-125 ' />
         </div>
     </div>
   )
