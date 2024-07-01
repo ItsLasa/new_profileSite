427e63eab68416b1186b77913709140cbb02b61b
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-01 23:46:21 +0530

    change heigth to auto

--- app/components/Languages.tsx
+++ app/components/Languages.tsx
@@ -1,3 +1,4 @@
+import { BorderBeam } from "./magicui/border-beam";
 import IconCloud from "./magicui/icon-cloud";
 
 const slugs = [
@@ -35,8 +36,9 @@ const slugs = [
 
 export default function IconCloudDemo() {
   return (
-    <div className="relative mt-4 flex h-full w-full max-w-[28rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-16 pb-20 pt-8 ">
+    <div className="relative mt-4 bg-gradient-to-br from-zinc-50 to-gray-50 flex h-auto shadow-lg  w-full max-w-[28rem] items-center justify-center overflow-hidden rounded-lg border bg-background px-16 pb-20 pt-8 ">
       <IconCloud iconSlugs={slugs} />
+      <BorderBeam size={250} duration={12} delay={9} />
     </div>
   );
 }
