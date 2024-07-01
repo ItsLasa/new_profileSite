30ac9f63ec5ed516e46606cc39e56f93db25060b
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-01 23:48:45 +0530

    add cool  bounce ball button

---
+++ app/components/CoolClickMe.tsx
@@ -0,0 +1,14 @@
+import { CoolMode } from "@/app/components/magicui/cool-mode";
+import React from "react";
+
+export default function CoolModeDemo() {
+  return (
+    <div className="relative justify-center">
+      <CoolMode>
+        <button className="bg-black animate-pulse  rounded-lg transition-all duration-300 text-white text-xs px-4 py-2 md:text-sm lg:px-4 lg:py-2 hover:scale-105">
+          Click me👻
+        </button>
+      </CoolMode>
+    </div>
+  );
+}
