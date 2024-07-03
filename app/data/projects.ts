d4751b076e2715ea688518b3767e6526092b0b80
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-03 21:51:33 +0530

    add git link

--- app/data/projects.ts
+++ app/data/projects.ts
@@ -5,13 +5,14 @@ export interface Project {
     image: string;
     link: string;
     details:string;
+    git:string;
   }
   
   const projects: Project[] = [
-    { id: 1, title: "Project One", image: "/todo1.jpg",link:"www.google.com", details:"Details About Project 1" },
-    { id: 2, title: "Project Two", image: "/todo2.png" ,link:"",details:"Details About Project 1" },
-    { id: 3, title: "Project Three", image: "/app3.png",link:""  ,details:"Details About Project 1"},
-    { id: 4, title: "Project Four", image: "/app4.jpg" ,link:"" ,details:"Details About Project 1"},
+    { id: 1, title: "Project One", image: "/todo1.jpg",link:"www.google.com", details:"Details About Project 1",git:"" },
+    { id: 2, title: "Project Two", image: "/todo2.png" ,link:"",details:"Details About Project 1" ,git:""},
+    { id: 3, title: "Project Three", image: "/app3.png",link:""  ,details:"Details About Project 1",git:""},
+    { id: 4, title: "Project Four", image: "/app4.jpg" ,link:"" ,details:"Details About Project 1",git:""},
    
   ];
   
