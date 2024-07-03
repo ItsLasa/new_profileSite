92039c15f310d15338dd924867d85b314209989f
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-03 21:51:13 +0530

    add git repo button

--- app/components/ProjectsCards.tsx
+++ app/components/ProjectsCards.tsx
@@ -1,7 +1,7 @@
 // components/ProjectCard.tsx
 import Image from "next/image";
 import { Project } from "../data/projects";
-import { FaLink } from "react-icons/fa";
+import { FaGithub, FaLink } from "react-icons/fa";
 import Link from "next/link";
 
 interface ProjectCardProps {
@@ -16,17 +16,23 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ project }) => {
         alt={project.title}
         width={400}
         height={200}
-        className="rounded-t-lg transition-all duration-300 hover:scale-105"
+        className="rounded-t-lg shadow-lg transition-all duration-300 hover:scale-105 hover:rounded-b-lg cursor-pointer "
       />
 
       <div className=" border-gray-300 pt-4 border-b-[1px] "></div>
 
       <div className="flex justify-between items-center px-2">
         <h3 className=" text-lg mt-4 mb-4">{project.title}</h3>
+        <div className="flex gap-4 items-center">
         <a href={project.link}>
-          <FaLink className="hover:scale-110  transition-all duration-300" />
+          <FaGithub className="hover:scale-125  transition-all duration-300" />
         </a>
+        <a href={project.link}>
+          <FaLink className="hover:scale-125  transition-all duration-300" />
+        </a>
+        </div>
       </div>
+      
       <div className="flex text-sm text-zinc-500 font-manrope justify-start items-center px-2">{project.details}</div>
     </div>
   );
