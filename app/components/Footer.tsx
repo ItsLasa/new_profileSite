d0782bbee929f07ab5169196c5ca2aa2d843719a
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-06 15:03:00 +0530

    hover style footer link

---
+++ app/components/Footer.tsx
@@ -0,0 +1,28 @@
+import Link from 'next/link'
+import React from 'react'
+import { FaInstagram } from 'react-icons/fa'
+import { FaCodepen, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa6'
+
+function Footer() {
+  return (
+    <div className='bg-black p-8 w-full h-auto text-white'>
+        <div className='flex flex-col justify-center items-center gap-4'>
+            <h1 className=''>Let’s
+            Connect.</h1>
+
+            <div className="mail">
+                Manojlasantha306@Gmail.com
+            </div>
+            <div className='flex justify-center items-center gap-4'>
+                <Link href={''}><FaGithub className=' transition-all duration-300 scale-110' /></Link>
+                <Link href={''}><FaLinkedin  className=' transition-all duration-300 scale-110'/></Link>
+                <Link href={''}><FaInstagram  className=' transition-all duration-300 scale-110'/></Link>
+                <Link href={''}><FaTwitter  className=' transition-all duration-300 scale-110'/></Link>
+                <Link href={''}><FaCodepen  className=' transition-all duration-300 scale-110'/></Link>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
