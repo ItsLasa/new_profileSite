3f0400cd434c3c3f6c9a59a242f5d10f405ee7d6
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-08 22:03:35 +0530

    coolclick me btn animated using tailwindcss animate bounce

--- app/components/CoolClickMe.tsx
+++ app/components/CoolClickMe.tsx
@@ -5,7 +5,7 @@ export default function CoolModeDemo() {
   return (
     <div className="relative justify-center">
       <CoolMode>
-        <button className="bg-black animate-pulse  rounded-lg transition-all duration-300 text-white text-xs px-4 py-2 md:text-sm lg:px-4 lg:py-2 hover:scale-105">
+        <button className="bg-black animate-bounce animate-infinite animate-ease-in-out animate-delay-100 rounded-lg transition-all duration-300 text-white text-xs px-4 py-2 md:text-sm lg:px-4 lg:py-2 hover:scale-105">
           Click me👻
         </button>
       </CoolMode>
