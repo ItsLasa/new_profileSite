42f454f699e855fa516878c960c13f68aa74a1ec
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-08 22:32:34 +0530

    add new NavBar

---
+++ app/components/Nav.tsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import { FloatingNav } from "../components/magicui/floating-navbar";
+import { IconHome, IconMessage, IconUser } from "@tabler/icons-react";
+export default function FloatingNavDemo() {
+  const navItems = [
+    {
+      name: "Home",
+      link: "/",
+      icon: <IconHome className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "Project",
+      link: "/projects",
+      icon: <IconUser className="h-4 w-4 text-neutral-500 dark:text-white" />,
+    },
+    {
+      name: "About Me",
+      link: "/about",
+      icon: (
+        <IconMessage className="h-4 w-4 text-neutral-500 dark:text-white" />
+      ),
+    },
+  ];
+  return (
+    <div className="relative  w-full">
+      <FloatingNav navItems={navItems} />
+      <DummyContent />
+    </div>
+  );
+}
+const DummyContent = () => {
+  return (
+    <main></main>
+  );
+};
