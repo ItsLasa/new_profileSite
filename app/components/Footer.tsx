bfdb01e5fd733495b792c7e7d14902b9bb84354f
Manoj Lasantha <94479824+ItsLasa@users.noreply.github.com>
2024-07-08 22:04:44 +0530

    hover footer link

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,20 +5,20 @@ import { FaCodepen, FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa6'
 
 function Footer() {
   return (
-    <div className='bg-black p-8 w-full h-auto text-white'>
-        <div className='flex flex-col justify-center items-center gap-4'>
-            <h1 className=''>Let’s
+    <div className='bg-black p-8 w-full font-manrope rounded-t-3xl  h-auto text-white'>
+        <div className='flex flex-col justify-center items-center gap-8'>
+            <h1 className='text-6xl font-franunce '>Let’s
             Connect.</h1>
 
             <div className="mail">
                 Manojlasantha306@Gmail.com
             </div>
             <div className='flex justify-center font-manrope items-center gap-4'>
-                <Link href={''}><FaGithub className=' transition-all duration-300 scale-110' /></Link>
-                <Link href={''}><FaLinkedin  className=' transition-all duration-300 scale-110'/></Link>
-                <Link href={''}><FaInstagram  className=' transition-all duration-300 scale-110'/></Link>
-                <Link href={''}><FaTwitter  className=' transition-all duration-300 scale-110'/></Link>
-                <Link href={''}><FaCodepen  className=' transition-all duration-300 scale-110'/></Link>
+                <Link href={''} passHref><FaGithub   className=' text-xl transition-all duration-300 hover:scale-110' /></Link>
+                <Link href={''}><FaLinkedin  className=' text-xl  transition-all duration-300 hover:scale-110'/></Link>
+                <Link href={''}><FaInstagram  className=' text-xl  transition-all duration-300 hover:scale-110'/></Link>
+                <Link href={''}><FaTwitter  className=' text-xl  transition-all duration-300 hover:scale-110'/></Link>
+                <Link href={''}><FaCodepen  className=' text-xl  transition-all duration-300 hover:scale-110'/></Link>
             </div>
         </div>
     </div>
